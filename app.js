44c0a979575a1ba99309359f334f5439debbff6c
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-06 17:07:06 -0600

    start mingooseDB connection

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-import mongoose from"mongoose";
-const uri = 'mongodb://127.0.0.1:27017/PeopleDB';
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+const uri = 'mongodb://127.0.0.1:27017/fruitsDB';
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const fruitSchema = new mongoose.Schema({
+        name: String,
+        rating: Number,
+        review: String
+      });
+      const Fruit = mongoose.model('Fruit', fruitSchema);
+      const fruit = new Fruit({ 
+        name: 'Apple',
+        rating: 7,
+        review: "Pretty Solid as  a fruit"
+     });
+     console.log(silence.name); 
+     await fruit.save();
+}
