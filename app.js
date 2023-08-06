e873be9afd5ecb939ac1c49aa6e8c48de9280154
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-06 17:43:11 -0600

    add express js module

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
 import mongoose from 'mongoose';
+
 const { Schema } = mongoose;
 const uri = 'mongodb://127.0.0.1:27017/fruitsDB';
-main().catch(err => console.log(err));
+
 async function main() {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const fruitSchema = new mongoose.Schema({
-        name: String,
-        rating: Number,
-        review: String
-      });
-      const Fruit = mongoose.model('Fruit', fruitSchema);
-      const fruit = new Fruit({ 
-        name: 'Apple',
-        rating: 7,
-        review: "Pretty Solid as  a fruit"
-     });
-     console.log(silence.name); 
-     await fruit.save();
+    try {
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const fruitSchema = new mongoose.Schema({
+            name: String,
+            rating: Number,
+            review: String
+        });
+        const Fruit = mongoose.model('Fruit', fruitSchema);
+
+        const kiwi = new Fruit({
+            name: "Kiwi",
+            rating: 10,
+            review: "The best fruit"
+        });
+
+        const orange = new Fruit({
+            name: "Orange",
+            rating: 10,
+            review: "Too sour for me"
+        });
+
+        const banana = new Fruit({
+            name: "Banana",
+            rating: 3,
+            review: "Weird texture"
+        });
+
+        await Fruit.insertMany([kiwi, orange, banana]);
+        console.log("Successfully saved all the fruits to fruits db");
+    } catch (error) {
+        console.error(error);
+    } finally {
+        mongoose.disconnect();
+    }
 }
+
+main().catch(err => console.error(err));
